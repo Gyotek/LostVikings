6e8269e6fc94c4cdffb7258e9e316e083be689cf
Gyotek <arthur.herbreteau@laposte.net>
2019-03-16 17:12:05 +0100

    new scene, add pont et bouton

---
+++ THE LOST VIKINGS/Assets/ARH_FOLDER/ARH_Scripts/ARH_Bouton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ARH_Bouton : MonoBehaviour
+{
+    public Animation animPont;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animPont.Play("Pont");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
