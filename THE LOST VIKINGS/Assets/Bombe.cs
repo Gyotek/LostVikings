718e258e38f22253fccc8f95f5c3f738a567bf11
Gyotek <arthur.herbreteau@laposte.net>
2019-04-11 22:09:35 +0200

    ADD bomb

--- THE LOST VIKINGS/Assets/Bombe.cs
+++ THE LOST VIKINGS/Assets/Bombe.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class Bombe : MonoBehaviour
 {
+
+    public Collider2D col;
     // Start is called before the first frame update
     void Start()
     {
-        
+        col.enabled = false;
+        StartCoroutine("Exploding");
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    IEnumerator Exploding()
+    {
+        yield return new WaitForSeconds(5f);
+        Debug.Log("KILL ALL");
+        col.enabled = true;
+        Destroy(this.gameObject);
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "enemy" || other.tag == "destructible")
+        {
+            Destroy(other.gameObject);
+        }
     }
 }
