63baad7668ccc714fea56cb2ce4476b7f498cd0d
Gago <cleamen@free.fr>
2019-04-12 15:18:23 +0200

    Added Erik Being Stunned after breaking a wall

--- THE LOST VIKINGS/Assets/BreakableWallBehavior.cs
+++ THE LOST VIKINGS/Assets/BreakableWallBehavior.cs
@@ -6,14 +6,24 @@ public class BreakableWallBehavior : MonoBehaviour
 {
 
     public XboxController erikController;
-    
+    public ErikAbility erikAbility;
+    public Rigidbody2D erikBody;
+
+    [SerializeField]
+    private float stunForce = 3.0f;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Erik") && erikController.speed >= 4 && erikController.isGrounded == true)
         {
-            //Stun Erik
+            StunErik();
             Destroy(gameObject);
         }
+    }
+
+    public void StunErik()
+    {
+        erikBody.AddForce(transform.up * stunForce, ForceMode2D.Impulse);
+        erikAbility.isStunned = true;
     }
 }
--- THE LOST VIKINGS/Assets/CLA_FOLDER/CLA_Scripts/ErikAbility.cs
+++ THE LOST VIKINGS/Assets/CLA_FOLDER/CLA_Scripts/ErikAbility.cs
@@ -13,6 +13,11 @@ public class ErikAbility : MonoBehaviour
     [SerializeField]
     private float thrust = 7;
 
+    public bool isStunned = false;
+
+    [SerializeField]
+    private float stunDelay = 1.0f;
+
     private void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
@@ -28,5 +33,18 @@ public class ErikAbility : MonoBehaviour
                 rigidBody.AddForce(transform.up * thrust, ForceMode2D.Impulse);
             }
         }
+
+        if (isStunned == true)
+        {
+            Debug.Log("omg I'm Erik and I'm Stunned by this wall!");
+            StartCoroutine(erikStunDelay());
+        }
+    }
+
+    IEnumerator erikStunDelay()
+    {
+        yield return new WaitForSeconds(stunDelay);
+        isStunned = false;
+        Debug.Log("I'm no longer stunned!");
     }
 }
