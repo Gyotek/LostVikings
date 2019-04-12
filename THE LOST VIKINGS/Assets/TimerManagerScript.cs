d29bffe91818dd879d4bf1f1bd06c139fb242f78
Louis Nillus <louis.nillus@gmail.com>
2019-04-12 23:50:42 +0200

    Working Timer

--- THE LOST VIKINGS/Assets/TimerManagerScript.cs
+++ THE LOST VIKINGS/Assets/TimerManagerScript.cs
@@ -1,22 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TimerManagerScript : MonoBehaviour
 {
     public float timeBeforeLoss = 120.0f;
 
-
+    public Text TimerText;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(second());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        TimerText.text = "Timer : " + timeBeforeLoss.ToString();
+
+        if (timeBeforeLoss <= 0)
+        {
+
+        }
+    } 
+
+    public IEnumerator second()
+    {
+        yield return new WaitForSeconds(1);
+        timeBeforeLoss--;
+        StartCoroutine(second());
     }
 }
