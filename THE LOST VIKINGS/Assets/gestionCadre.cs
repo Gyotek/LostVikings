7d717ee0f4e9b48fd2780d6dd426f2ad4af1c656
vandermeirssche <p.vandermeirssche@rubika-edu.com>
2019-04-12 14:35:48 +0200

    Ui icone + Sprites rogné

--- THE LOST VIKINGS/Assets/gestionCadre.cs
+++ THE LOST VIKINGS/Assets/gestionCadre.cs
@@ -1,20 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class gestionCadre : MonoBehaviour
 {
     public IconManager _iconManager;
+    public IconManager _iconManagerOlaf;
+    public IconManager _iconManagerErik;
+    public IconManager _iconManagerBaleog;
+
     public InventorySystem _inventorySystem;
 
-    private int numero = 0;
+    private int numero;
 
     public GameObject iconeSelected;
-    public string iconeName;
+
     public GameObject icone0;
     public GameObject icone1;
     public GameObject icone2;
     public GameObject icone3;
+
+    private XboxController _xboxController;
+
+    public GameObject olaf;
+    public GameObject erik;
+    public GameObject baleog;
 
 
 
@@ -26,21 +37,48 @@ public class gestionCadre : MonoBehaviour
 
     void Update()
     {
-        //iconeName = icone1;
 
-        numero = Mathf.Clamp(numero, 0, 3);
+        //numero = _iconManager.iconeSelectNum;
+        numero = Mathf.Clamp(_iconManager.iconeSelectNum, 0, 3);
 
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            numero -= 1;
-            iconeSelected = GameObject.Find("Icon" + numero);
+            if (erik.GetComponent<XboxController>().thisIsSelected == true )
+            {
+                iconeSelected = GameObject.Find("InventoryErik/Panel/Slot1/Icon" + _iconManagerErik.iconeSelectNum);
+            }
+
+            if (olaf.GetComponent<XboxController>().thisIsSelected == true)
+            {
+                iconeSelected = GameObject.Find("InventoryOlaf/Panel/Slot1/Icon" + _iconManagerOlaf.iconeSelectNum);
+            }
+
+            if (baleog.GetComponent<XboxController>().thisIsSelected == true)
+            {
+                iconeSelected = GameObject.Find("InventoryBaleog/Panel/Slot1/Icon" + _iconManagerBaleog.iconeSelectNum);
+            }
+
             this.transform.position = iconeSelected.transform.position;
+
         }
 
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            numero += 1;
-            iconeSelected = GameObject.Find("Icon" + numero);
+            if (erik.GetComponent<XboxController>().thisIsSelected == true)
+            {
+                iconeSelected = GameObject.Find("InventoryErik/Panel/Slot1/Icon" + _iconManagerErik.iconeSelectNum);
+            }
+
+            if (olaf.GetComponent<XboxController>().thisIsSelected == true)
+            {
+                iconeSelected = GameObject.Find("InventoryOlaf/Panel/Slot1/Icon" + _iconManagerOlaf.iconeSelectNum);
+            }
+
+            if (baleog.GetComponent<XboxController>().thisIsSelected == true)
+            {
+                iconeSelected = GameObject.Find("InventoryBaleog/Panel/Slot1/Icon" + _iconManagerBaleog.iconeSelectNum);
+            }
+
             this.transform.position = iconeSelected.transform.position;
         }
     }
