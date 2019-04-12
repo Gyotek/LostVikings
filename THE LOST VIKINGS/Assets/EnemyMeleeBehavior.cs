38025760e96a0a78a9540c8181fabfb7e5056b60
Louis Nillus <louis.nillus@gmail.com>
2019-04-12 20:38:26 +0200

    Fucked melee enemy
    I'ts broken

---
+++ THE LOST VIKINGS/Assets/EnemyMeleeBehavior.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMeleeBehavior : MonoBehaviour
+{
+
+    public float speed;
+
+    [SerializeField]
+    private float raycastDistance = 3.0f;
+
+    private bool movingRight = true;
+
+    public Transform groundDetection;
+
+    public bool playerInRange = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+
+        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, raycastDistance);
+        if(groundInfo.collider == false)
+        {
+            if (movingRight == true)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                movingRight = false;
+            }
+
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                movingRight = true;
+            }
+        }
+
+        Debug.DrawRay(groundDetection.position, Vector2.down * raycastDistance);
+
+        if (playerInRange == true)
+        {
+            Debug.Log("I see an enemy!");
+        }
+;    }
+}
---
+++ THE LOST VIKINGS/Assets/MeleeCollision.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeCollision : MonoBehaviour
+{
+
+    public EnemyMeleeBehavior selfViking;
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Characters"))
+        {
+            Debug.Log("Entering Collision");
+            selfViking.playerInRange = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Characters"))
+        {
+            Debug.Log("Exiting Collision");
+            selfViking.playerInRange = true;
+        }
+    }
+
+}
