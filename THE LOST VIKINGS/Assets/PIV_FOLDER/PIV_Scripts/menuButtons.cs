78289e2fa8f37e91b60ccfe342ea33705a906273
vandermeirssche <p.vandermeirssche@rubika-edu.com>
2019-04-12 15:33:33 +0200

    Menu

---
+++ THE LOST VIKINGS/Assets/PIV_FOLDER/PIV_Scripts/menuButtons.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+    
+    public class menuButtons : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void launchScene()
+    {
+        SceneManager.LoadScene("LON_Scene 1");
+        Debug.Log("Scene loaded");
+    }
+
+    public void exitGame()
+    {
+        Application.Quit();
+        Debug.Log("game closed");
+    }
+
+}
